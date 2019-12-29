2eede10bbef0d16c4d04d9c733bac20620100f3a
BuildTools <unconfigured@null.spigotmc.org>
2019-12-28 19:13:06 -0500

    Improve text

--- src/views/base/Layout.vue
+++ src/views/base/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app class="primary">
     <Header></Header>
       <v-content>
           <router-view/>
--- src/views/base/components/Footer.vue
+++ src/views/base/components/Footer.vue
@@ -17,14 +17,14 @@
         </v-btn>
       </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      <v-card-text class="secondary--text pt-0">
+        Site web fait par Karl-Étienne Bolduc & Pascal Vaillancourt (mais principalement Karl...) pour LAN, inc. 
       </v-card-text>
 
       <v-divider></v-divider>
 
-      <v-card-text class="white--text">
-        &copy;2018 — <strong>Vuetify</strong>
+      <v-card-text class="grey--text">
+        2020
       </v-card-text>
     </v-card>
     </v-flex>
