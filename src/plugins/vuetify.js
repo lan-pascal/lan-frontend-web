c655dfe7fc1494ba3349b37ebba2b80ea83b1461
LodrikMtl <26026137+LodrikMtl@users.noreply.github.com>
2020-01-04 16:44:04 -0500

    Update Vuetify Variable

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,11 +5,27 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
     theme: {
+        options: {
+          customProperties: true
+        },
+        dark: true,
         themes: {
-          light: {
-            primary: "#000000",
-            secondary: "#FFFFFF",
+          dark: {
+            primary: {
+              base: "#252525",
+              lighten1: "#616161",
+              darken1: "#202020",
+              darken2: "#080808"
+            },
+            secondary: {
+              base: "#d6d6d6",
+              lighten1: "#e0e0e0",
+              darken1: "#8f8f8f"
+            },
+            accent: {
+              base: "#eb7134"
+            } 
           },
         }
-    }
+    },
 });
