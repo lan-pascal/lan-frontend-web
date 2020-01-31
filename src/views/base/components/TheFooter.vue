f283bc80fcd22816ca6d543b02b52192d5453095
kwan-s <stephenkwan07@gmail.com>
2020-01-31 13:54:19 -0500

    Update footer.

--- src/views/base/components/TheFooter.vue
+++ src/views/base/components/TheFooter.vue
@@ -1,30 +1,9 @@
 <template>
   <v-footer padless>
     <v-flex>
-      <v-card
-        text
-        tile
-        class="primary lighten-1 white--text text-xs-center"
-      >
-        <v-card-text>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-3 white--text"
-            icon
-          >
-            <v-icon size="24px">{{ icon }}</v-icon>
-          </v-btn>
-        </v-card-text>
-
-        <v-card-text class="secondary--text pt-0">
-          Site web fait par Karl-Étienne Bolduc & Pascal Vaillancourt (mais principalement Karl...) pour LAN, inc. 
-        </v-card-text>
-
-        <v-divider></v-divider>
-
-        <v-card-text class="grey--text">
-          2020
+      <v-card text tile class="primary lighten-1 white--text text-xs-center">
+        <v-card-text class="grey--text text-center">
+          <span>LAN © 2018 - 2020</span>
         </v-card-text>
       </v-card>
     </v-flex>
@@ -32,12 +11,10 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-    })
-  }
+export default {
+  data: () => ({})
+};
 </script>
 
 <style>
-
 </style>
