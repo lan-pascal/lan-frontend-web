59ba9911e621a1b82297bc569757c958c56b422b
BuildTools <unconfigured@null.spigotmc.org>
2020-03-03 17:57:58 -0500

    Change to TheFAQ. Questions, answers and categories.

--- src/views/base/TheFAQ.vue
+++ src/views/base/TheFAQ.vue
@@ -72,89 +72,78 @@ export default {
       selection: "Tous",
       faqs: [
         {
-          categories: ["Populaire"],
-          question: "Où sont les informations sur le prochain LAN?",
+          categories: ["Aide"],
+          question: "Où sont les informations sur le prochain Quart de nuit?",
           answer: "Clique ici."
         },
         {
-          categories: ["Populaire"],
-          question: "Je veux aider avec le LAN, comment faire?",
+          categories: ["Aide"],
+          question: "Je ne comprends pas trop c’est quoi le Quart de nuit.",
           answer:
-            "Cool! Contacte-nous ici. Peu importe ce que tu sais faire, on est veut connaître ton intérêt! Souvent aussi, on cherche du matériel, donc tu peux nous faire part de ce que tu as (surtout en matière jeux vidéo, jeux de société)"
+            "Le Quart de nuit c'est une soirée que tu passes avec tes amis à faire ce que tu as le goût dans toutes les activités offertes (clique ici pour la liste des activités)! Tu es libre d'aller jouer à ce que tu veux et quand tu veux. Tu n'es même pas obligé de jouer, plusieurs viennent simplement socialiser. C'est aussi l'occasion parfaite pour rencontrer plein de nouveau monde. Et si tu penses que c'est pas pour toi parce que tes pas nerd ou geek, tu peux pas avoir moins raison, on y trouve de tous les goûts : jeux de sociétés, karaoke, trivia, bar, jeux vidéo accessibles (Just Dance, Dance Dance Revolution, Guitar Hero, etc.) et plus!"
         },
-        {
-          categories: ["Misc"],
-          question:
-            "J’ai des suggestions pour le prochain LAN/le futur du LAN. Comment les partager?",
-          answer: "Super! Vas ici pour nous en faire part."
-        },
-        {
-          categories: ["Misc"],
-          question: "Je ne comprends pas trop c’est quoi le LAN.",
-          answer:
-            "Le LAN c'est une soirée que tu passes avec tes amis à faire ce que tu as le goût dans toutes les activités offertes (voir liste ci-bas)! Tu es libre d'aller jouer à ce que tu veux et quand tu veux. Tes même pas obligé de jouer, plusieurs viennent simplement socialiser. C'est aussi l'occasion parfaite pour rencontrer plein de nouveau monde. Et si tu penses que c'est pas pour toi parce que tes pas nerd, tu peux pas avoir moins raison, on y trouve de tous les goûts : jeux de sociétés, jeux vidéo accessibles (Just Dance, Dance Dance Revolution, Guitar Hero, etc.), karaoke & plus!"
+         {
+          categories: ["Paiement", "Aide"],
+          question: "Où puis-je acheter mon billet?",
+          answer: "Clique ici."
         },
         {
           categories: ["Paiement"],
-          question: "C’est où que j’achète mon billet?",
-          answer: "Clique ici."
-        },
-        {
-          categories: ["Populaire", "Paiement"],
           question: "Quelle méthode de paiement acceptez-vous?",
           answer: "On accepte […]"
         },
-        {
+         {
           categories: ["Paiement"],
           question: "Puis-je me faire rembourser une fois mon billet acheté?",
           answer:
             "Normalement, toute vente est finale. Mais on est des humains et on comprend que ça arrive des empêchement dernière minute. Si tu crois que tu as une bonne raison pour te faire rembourser (garde en tête qu’on roule pas sur l’or et que donc nos option sont limitées), contacte-nous ici"
         },
         {
-          categories: ["Populaire", "Misc"],
-          question: "Acceptez-vous les dons?",
-          answer: "De l’argent???? Oui!! Clique ici (xoxoxoxo)."
-        },
-        {
-          categories: ["Misc"],
-          question: "Avez-vous l’histoire des LANs?",
-          answer: "Certainement, clique ici!"
-        },
-        {
           categories: ["Paiement"],
           question: "Puis-je acheter un billet sur place?",
           answer:
-            "Malheureusement, à cause des complications que cela engendrerait non. Mais il est possible, qu’ils restent des billets. Si c’est le cas, ce sera indiqué sur le site et les médias sociaux."
+            "Malheureusement, à cause des complications que cela engendrerait : non. Mais il est possible, qu’ils restent des billets. Si c’est le cas, ce sera indiqué sur le site et les médias sociaux."
         },
         {
-          categories: ["Information"],
-          question: "Comment rester au courant avec le LAN?",
+          categories: ["Divers"],
+          question: "Puis-je apporter mon alcool et/ou ma nourriture?",
           answer:
-            "Tu peux t’abonner à notre info-lettre ici ou nous suivre sur les diverse plateformes que nous avons ici. Le site web est aussi souvent mis-à-jour, donc reviens de temps en temps!"
+            "Ça dépend d'année en année (notamment de l'organisme qui nous loue la salle). Pour le Quart de Nuit 5, il est interdit d'apporter de l'alcool, mais permis d'apporter de la nourriture. Clique ici pour savoir pour le prochain LAN."
         },
         {
-          categories: ["Misc"],
-          question: "Comment s’inscrire aux tournois?",
-          answer: "Quand tu achètes ton billet, l’option se présente à toi."
-        },
-        {
-          categories: ["Tournoi"],
-          question:
-            "J’ai déjà acheté mon billet, puis-je tout de même m’ajouter à un tournoi?",
-          answer: "Ça dépend des disponibilités. Contacte-nous ici."
-        },
-        {
-          categories: ["Misc"],
+          categories: ["Divers", "Contribuer"],
           question:
             "Puis-je apporter mon ordinateur, ma console de jeu et/ou des jeux de société ?",
           answer:
-            "Tu peux apporter ton ordinateur portable sans problème (par contre, tu en es responsable). Pour les ordinateurs « tours », il faut que tu vérifies avec nous avant en nous contactant ici et c’est toi qui  s’occupe d’apporter ton écran, clavier, souris, etc. Pour les consoles de jeu, ça dépend la disponibilité des télévisions, contacte-nous ici. Bien sûr les consoles de jeu portables sont les bienvenues! Pour les jeux de société, pas de problème!"
+            "Tu peux apporter ton ordinateur portable sans problème (par contre, tu en es responsable). Pour les ordinateurs « tours », il faut que tu vérifies avec nous avant en nous contactant ici et c’est toi qui s’occupe d’apporter ton écran, clavier, souris, etc. Pour les consoles de jeu, ça dépend la disponibilité des télévisions, contacte-nous ici. Bien sûr les consoles de jeu portables sont les bienvenues! Pour les jeux de société, pas de problème (mais tu en es responsable aussi)!"
         },
         {
-          categories: ["Misc"],
-          question: "Puis-je apporter mon alcool et/ou ma nourriture?",
+          categories: ["Contribuer"],
+          question: "Je veux aider avec le Quart de nuit, comment faire?",
           answer:
-            "Ça dépend de LAN en LAN (notamment de la personne qui nous loue la salle). Clique ici pour savoir pour le prochain LAN."
+            "Cool! Contacte-nous ici. Peu importe ce que tu sais faire, on est veut connaître ton intérêt! Souvent aussi, on cherche du matériel, donc tu peux nous faire part de ce que tu as (surtout en matière jeux vidéo, jeux de société)"
+        },
+        {
+          categories: ["Contribuer"],
+          question:
+            "J’ai des suggestions pour le prochain Quart de nuit/le futur du Quart de nuit. Comment les partager?",
+          answer: "Super! Vas ici pour nous en faire part."
+        },
+        {
+          categories: ["Contribuer"],
+          question: "Acceptez-vous les dons?",
+          answer: "De l’argent??? Oui!! Clique ici (xoxoxoxo)."
+        },
+        {
+          categories: ["Divers"],
+          question: "Est-il possible de consulter le passé/archives de Quart de nuit?",
+          answer: "Certainement, clique ici!"
+        },
+        {
+          categories: ["Divers"],
+          question: "Comment rester au courant avec le LAN?",
+          answer:
+            "Clique ici pour voir tous nos réseaux sociaux. Le site web est aussi souvent mis-à-jour, donc reviens de temps en temps!"
         }
       ]
     };
