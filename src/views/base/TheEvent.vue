ff12060ebb13285d57ae9aec3322978233b1e383
nclsaudet <nclsaudet@gmail.com>
2020-03-03 16:57:46 -0500

    Event page quick layout

--- src/views/base/TheEvent.vue
+++ src/views/base/TheEvent.vue
@@ -1,40 +1,113 @@
 <template>
-    <v-col>
-      <v-row class="my-3">
-            <v-col align=center justify=center>
-                <span class="display-3 accent--text">Historique</span>
-            </v-col>
-      </v-row>
+  <div class="justify-center primary">
 
+    <v-row class="primary pa-4">
+      <v-img 
+      src="../../assets/logo/QDN2020_Artwork.png"
+      width="450"
+      contain
+      ></v-img>
+      <h2 class="accent--text">Ouverture des portes dès 20h</h2>
+    </v-row>
+
+    <v-row class="primary">
+      <v-col>
+        <v-row>
+          <h1>Le QdN, c'est quoi<span class="accent--text rotate">?</span></h1>
+        </v-row>
+        <v-row>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quisquam minus, 
+          voluptatem dolorum repudiandae veniam fugiat quae minima vero porro dolore atque, 
+          voluptatum impedit laudantium magni dolores. Natus, in quas.
+        </p>
+        </v-row>
+      </v-col>
+    </v-row>
+
+    <v-row class="primary">
+      <v-col>
+        <v-row>
+        <h1 class="accent--text">SCÈNE</h1>
+      </v-row>
       <v-row>
-        <v-timeline id="event-timeline">
-
-          <v-timeline-item v-for="(event, eventIndex) in events" :key="eventIndex">
-                <template v-slot:opposite>
-                <span>{{event.date}} - {{event.place}}</span>
-                </template>
-
-                <v-card class="elevation-2">
-                    <v-card-title class="headline">{{event.name}}</v-card-title>
-                    <v-card-text>
-                        <span class="d-block">Nombre de participants: {{event.numberParticipants}}</span>
-                        <template v-for="(highlight, highlightIndex) in event.highlights">
-                            <span class="d-block" :key="highlightIndex">- {{highlight.item}}</span>
-                        </template>
-                    </v-card-text>
-                </v-card>
-
-          </v-timeline-item>
-
-        </v-timeline>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quisquam minus, 
+        voluptatem dolorum repudiandae veniam fugiat quae minima vero porro dolore atque, 
+        voluptatum impedit laudantium magni dolores. Natus, in quas.
+        </p>
       </v-row>
-    </v-col>
+      </v-col>
+      <v-timeline>
+        <v-timeline-item
+        v-for="(activity, index) in schedule"
+        :key="index"
+        small
+        right>
+          <template v-slot:opposite>
+            <h3
+            v-text="activity.time"></h3>
+          </template>
+          <div class="py-4">
+            <h3>{{activity.name}}</h3>
+          </div>
+        </v-timeline-item>
+      </v-timeline>
+    </v-row>
+
+    <v-row
+    class="primary"
+    align="center">
+      <v-col>
+        <v-row>
+          <h1 class="accent--text">SCÈNE</h1>
+        </v-row>
+        <v-row>
+          <v-img src="../../assets/media/board-game-stack.svg"></v-img>
+        </v-row>  
+      </v-col>
+    </v-row>
+
+    <v-row
+    class="primary"
+    align="center">
+      <v-card>
+
+      </v-card>
+    </v-row>
+  </div>
+    
 </template>
+<style lang="scss">
+.rotate {
+  transform: rotate(20deg);
+}
+
+
+</style>
+
 
 <script>
 export default {
   name: "event",
   components: {},
+  data() {
+    return {
+      schedule: [
+        {name: "Rock Band & Guitar Hero", time: "20h"},
+        {name: "Trivia Quiz", time: "21h"},
+        {name: "", time: ""},
+        {name: "Karaoke", time: "23h"},
+        {name: "", time: ""},
+        {name: "Tournois de Dance Dance Revolution", time: "01h"},
+        {name: "", time: ""},
+        {name: "Rock Band & Guitar Hero", time: "03h"},
+      ],
+      tournament: [
+        {name: "TETRIS", time: "21h", location: "Loundge", vector:"../../assets/media/tetris-vector.svg"},
+        {name: "SUPER SMASH BROS.", time: "23h", location: "Loundge", vector:"../../assets/media/ssb-vector.svg"},
+        {name: "DANCE DANCE REVOLUTION", time: "01h", location: "Scène principale", vector:"../../assets/media/board-game-stack.svg"}
+      ]
+    }
+  }
 
 }
 </script>
