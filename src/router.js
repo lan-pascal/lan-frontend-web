e10576a2a696a0563633586e5eb24f75e6e13fee
LodrikMtl <karl-etienne.bolduc@outlook.com>
2020-03-10 20:42:56 -0400

    Update scroll behavior

--- src/router.js
+++ src/router.js
@@ -10,8 +10,11 @@ const router = new Router({
   mode: 'history',
   routes: [
   ].concat(base).concat(registration),
-  scrollBehavior(){
-    return { x: 0, y: 0 }
+  scrollBehavior(to, from, savedPosition){
+    if (savedPosition) {
+      return savedPosition
+    } else
+      return { x: 0, y: 0 }
   }
 
 })
