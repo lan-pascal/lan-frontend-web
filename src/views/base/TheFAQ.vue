4180e882b019b17f3fc5ac269c44bd6d92251f7a
BuildTools <unconfigured@null.spigotmc.org>
2020-03-13 11:19:49 -0400

    Corrects FAQ

--- src/views/base/TheFAQ.vue
+++ src/views/base/TheFAQ.vue
@@ -84,7 +84,7 @@ export default {
           categories: ["Aide"],
           question: "Je ne comprends pas trop c’est quoi le Quart de nuit.",
           answer:
-            "Le Quart de nuit c'est une soirée que tu passes avec tes amis à faire ce que tu as le goût dans toutes les activités offertes (clique ici pour la liste des activités)! Tu es libre d'aller jouer à ce que tu veux et quand tu veux. Tu n'es même pas obligé de jouer, plusieurs viennent simplement socialiser. C'est aussi l'occasion parfaite pour rencontrer plein de nouveau monde. Et si tu penses que c'est pas pour toi parce que tes pas nerd ou geek, tu peux pas avoir moins raison, on y trouve de tous les goûts : jeux de sociétés, karaoke, trivia, bar, jeux vidéo accessibles (Just Dance, Dance Dance Revolution, Guitar Hero, etc.) et plus!"
+            "Le Quart de nuit c'est une soirée que tu passes avec tes amis à faire ce que tu as le goût dans toutes les activités offertes (clique ici pour la liste des activités)! Tu es libre de faire ce que tu veux, quand tu veux. Tu n'es même pas obligé.e de jouer : plusieurs viennent simplement pour socialiser. C'est aussi l'occasion parfaite pour rencontrer plein de nouveau monde. Et si tu penses que ce n’est pas pour toi parce que tu n’es pas nerd ou geek, tu ne peux pas avoir moins raison, on y trouve de tous les goûts : jeux de sociétés, karaoke, trivia, bar, jeux vidéo accessibles (Just Dance, Dance Dance Revolution, Guitar Hero, etc.) et plus!"
         },
          {
           categories: ["Paiement", "Aide"],
@@ -100,38 +100,38 @@ export default {
           categories: ["Paiement"],
           question: "Puis-je me faire rembourser une fois mon billet acheté?",
           answer:
-            "Normalement, toute vente est finale. Mais on est des humains et on comprend que ça arrive des empêchement dernière minute. Si tu crois que tu as une bonne raison pour te faire rembourser (garde en tête qu’on roule pas sur l’or et que donc nos option sont limitées), contacte-nous <a href='mailto:quartdenuitmtl@gmail.com'>ici</a>."
+            "Normalement, toute vente est finale. Toutefois, on est des humains et on comprend que ça arrive des empêchements dernière minute. Si tu crois que tu as une bonne raison pour te faire rembourser (garde en tête qu’on ne roule pas sur l’or et donc que nos options sont limitées), contacte-nous <a href='mailto:quartdenuitmtl@gmail.com'>ici</a>."
         },
         {
           categories: ["Paiement"],
           question: "Puis-je acheter un billet sur place?",
           answer:
-            "Malheureusement, à cause des complications que cela engendrerait : non. Mais il est possible, qu’ils restent des billets. Si c’est le cas, ce sera indiqué sur le site et les médias sociaux (tous nos réseaux sociaux sont au bas de la page)."
+            "Malheureusement, à cause des complications que cela engendrerait : non. Par contre, il est possible qu’il reste des billets le jour de l’événement. Si c’est le cas, ce sera indiqué sur le site et sur les médias sociaux."
         },
         {
           categories: ["Paiement"],
           question: "Pourquoi est-ce si cher?",
           answer:
-            "Le Quart de nuit est un événement assez unique en son genre simplement par ce qu'il offre. Alors, avoir tout cet équipement (TVs, consoles, jeux, karaoké, bar, services, etc.) et organiser tout cela prend beaucoup de temps et d'argent. C'est sans oublier la réservation de salle! Croyez-nous, si on pouvait réduire les prix, on le ferait. Dites-vous qu'en achetant votre billet, vous n'avez pas seulement une place pour le prochain Quart, mais vous aidez à garantir son futur."
+            "Le Quart de nuit est un événement assez unique en son genre simplement par ce qu'il offre. Avoir tout cet équipement (TVs, consoles, jeux, karaoké, bar, services, etc.) et organiser tout cela prend beaucoup de temps et d'argent. C'est sans oublier la réservation de salle! Croyez-nous, si on pouvait réduire les prix, on le ferait. Dites-vous qu'en achetant votre billet, vous n’investissez pas seulement dans une place pour le prochain Quart, mais vous aidez à garantir son futur."
         },
         {
           categories: ["Divers"],
           question: "Puis-je apporter mon alcool et/ou ma nourriture?",
           answer:
-            "Ça dépend d'année en année (notamment de l'organisme qui nous loue la salle). Pour le Quart de Nuit V, il est interdit d'apporter de l'alcool, mais permis d'apporter de la nourriture. L'eau est permise, mais seulement dans un contenant hermétique."
+            "Ça dépend d'année en année (notamment selon l'organisme qui nous loue la salle). Pour le Quart de Nuit 5, il est interdit d'apporter de l'alcool, mais permis d'apporter de la nourriture. Clique ici pour t’informer par rapport au prochain Quart de nuit."
         },
         {
           categories: ["Divers", "Contribuer"],
           question:
             "Puis-je apporter mon ordinateur, ma console de jeu et/ou des jeux de société ?",
           answer:
-            "Tu peux apporter ton ordinateur portable sans problème (par contre, tu en es responsable). Pour les ordinateurs « tours », <br/> il faut que tu vérifies avec nous avant en nous contactant ici et c’est toi qui s’occupe d’apporter ton écran, clavier, souris, etc. Pour les consoles de jeu, ça dépend la disponibilité des télévisions, contacte-nous <a href='mailto:quartdenuitmtl@gmail.com'>ici</a>. Bien sûr les consoles de jeu portables sont les bienvenues! Pour les jeux de société, pas de problème (mais tu en es responsable aussi)!"
+            "Pour les ordinateurs portables et les jeux de société, et les consoles de jeux portables, il n’y a aucun problème. Pour les ordinateurs « tours » (incluant tous les équipements connexes tels que la souris, le clavier, l’écran, etc.), ainsi que pour les consoles de jeu, il est important de vérifier avec nous avant l’événement. N’oublie pas que tu es entièrement responsable de tous les items que tu amènes. "
         },
         {
           categories: ["Contribuer"],
           question: "Je veux aider avec le Quart de nuit, comment faire?",
           answer:
-            "Cool! Contacte-nous <a href='mailto:quartdenuitmtl@gmail.com'>ici</a>. Peu importe ce que tu sais faire, on est veut connaître ton intérêt! Souvent aussi, on cherche du matériel, donc tu peux nous faire part de ce que tu as (surtout en matière jeux vidéo, jeux de société)"
+            "Cool! Contacte-nous <a href='mailto:quartdenuitmtl@gmail.com'>ici</a>. Peu importe ce que tu sais faire, on est ouvert! De plus, on cherche souvent du matériel, donc tu peux nous faire part de ce que tu as (surtout en matière de jeux vidéo, de jeux de société)"
         },
         {
           categories: ["Contribuer"],
