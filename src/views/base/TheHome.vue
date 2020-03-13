a68763d8a4a6cd9078b37f16d4a598614f7c21fe
nclsaudet <nclsaudet@gmail.com>
2020-03-13 15:09:29 -0400

    Complete Logo Color Fix

--- src/views/base/TheHome.vue
+++ src/views/base/TheHome.vue
@@ -110,7 +110,7 @@
           </p>
         </v-row>
         <v-row justify="center">
-          <v-img class="ma-6" aspect-ratio="0.8" max-width="275"></v-img>
+          <v-img contain class="ma-6" aspect-ratio="0.8" max-width="300" src="@/assets/media/activities/game-board-stack.svg"></v-img>
         </v-row>
       </v-col>
     </v-row>
@@ -332,7 +332,8 @@
 
 <script>
 import QdnTitleSVG from "@/assets/logo/qdn-logo-vector.svg?inline"
-import LogoSVG from "@/assets/logo/logo-noir.svg?inline";
+import LogoSVG from "@/assets/logo/logo-bleu.svg?inline";
+//import GameStackSVG from "@/assets/media/activities/game-board-stack.svg?inline"
 import Countdown from "@/components/countdown/CountdownNormal";
 
 export default {
