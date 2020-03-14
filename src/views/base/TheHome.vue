607666f95ff370cd077aee4a78cfe3856231cf3e
LodrikMtl <karl-etienne.bolduc@outlook.com>
2020-03-14 16:05:54 -0400

    Remove unecessary methods

--- src/views/base/TheHome.vue
+++ src/views/base/TheHome.vue
@@ -332,16 +332,6 @@ export default {
       ]
     };
   },
-  methods: {
-    next() {
-      this.onboarding =
-        this.onboarding + 1 === this.length ? 0 : this.onboarding + 1;
-    },
-    prev() {
-      this.onboarding =
-        this.onboarding - 1 < 0 ? this.length - 1 : this.onboarding - 1;
-    }
-  }
 };
 </script>
 
